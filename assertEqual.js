85aeb0535b4149bde6b08c6f0a5ed38a3365de4f
Ali Shirmohammadi <ali.shirmohammadi1997@gmail.com>
2023-05-21 13:44:05 -0700

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// const actual = "Lighthouse Labs";
+// const expected = "Bootcamp";
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } /* if (actual !== expected) */ else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual(actual, expected);
+assertEqual("✅✅✅", "🛑🛑🛑");
+assertEqual(1, 1);
