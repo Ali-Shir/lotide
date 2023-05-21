9d6ae656f4719be73b32b963f7c5ba17b18d380b
Ali Shirmohammadi <ali.shirmohammadi1997@gmail.com>
2023-05-21 15:21:40 -0700

    Created a function which returns the first item in the array

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("LHL", "Bootcamp");
+assertEqual(1, 1);
+
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  }
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
