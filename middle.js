c826167f87c2968f85078d98bdf90b1c3aec284f
Ali Shirmohammadi <ali.shirmohammadi1997@gmail.com>
2023-05-25 22:49:59 -0700

    Implemented middle.js for Lotide

---
+++ middle.js
@@ -0,0 +1,43 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function assertArrayEqual(arr1, arr2) {
+  const check = eqArrays(arr1, arr2);
+  if (check) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const middle = function (array) {
+  const arrLength = array.length;
+  if (arrLength <= 2) {
+    return [];
+  }
+  const middleIndex = Math.floor(arrLength / 2);
+  if (arrLength % 2 === 0) {
+    return [array[middleIndex - 1], array[middleIndex]];
+  } else {
+    return [array[middleIndex]];
+  }
+};
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
