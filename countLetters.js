a4c27c0b5a1f247a8e2ca3950ed66c4284751b1e
Ali Shirmohammadi <ali.shirmohammadi1997@gmail.com>
2023-06-03 02:50:39 -0700

    Implemented countLetters.js for Lotide

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+
+  for (const letter of sentence) {
+    if(letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const letterCounts = countLetters("Ali Shirmohammadi");
+console.log(letterCounts);
