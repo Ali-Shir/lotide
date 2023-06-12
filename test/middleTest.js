6cd01e76bb60591410bcbf1a4e62033dbae1da29
Ali Shirmohammadi <ali.shirmohammadi1997@gmail.com>
2023-06-12 01:37:46 -0700

    Refactor with Mocha and Chai in Lotide middleTest

---
+++ test/middleTest.js
@@ -0,0 +1,57 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function assertArrayEqual(arr1, arr2) {
+  const check = eqArrays(arr1, arr2);
+  if (check) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const middle = function (array) {
+  const arrLength = array.length;
+  if (arrLength <= 2) {
+    return [];
+  }
+  const middleIndex = Math.floor(arrLength / 2);
+  if (arrLength % 2 === 0) {
+    return [array[middleIndex - 1], array[middleIndex]];
+  } else {
+    return [array[middleIndex]];
+  }
+};
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
